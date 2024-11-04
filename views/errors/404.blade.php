c7790d41f7127030c8b504d26237b3476580cb6a
Oleg <114818807+10alexandrov@users.noreply.github.com>
2024-11-04 11:41:27 +0100

    Update 404.blade.php

--- views/errors/404.blade.php
+++ views/errors/404.blade.php
@@ -1,8 +1,11 @@
-@extends('errors.layout')
-@section('content')
+<div class="form-floating mb-3">   <!-- old -->
+                                <input id="user" type="text" class="form-control @error('email') is-invalid @enderror"
+                                    name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <label for="user">{{ __('User') }}</label>
 
-    <div class="container text-center">
-        <h1>La página a la que quieres acceder no existe</h1>
-    </div>
-
-@endsection
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
